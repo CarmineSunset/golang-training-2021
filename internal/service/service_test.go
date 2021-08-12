ad24b7bb5a8e268d1212c6b02dd74aede9658d03
Alexander Kedrik <alkedr@yandex-team.ru>
2021-08-12 11:43:27 +0300

    create valid test

--- internal/service/service_test.go
+++ internal/service/service_test.go
@@ -1,11 +1,14 @@
 package service_test
 
 import (
-	// "testing"
+	"testing"
+	"time"
+	"fmt"
 
 
 	"github.com/andreipimenov/golang-training-2021/internal/service"
 	"github.com/andreipimenov/golang-training-2021/internal/mock"
+	"github.com/andreipimenov/golang-training-2021/internal/model"
 
 
 	
@@ -21,6 +24,13 @@ type serviceTestSuite struct {
 
 }
 
+const (
+	ticker      = "AAPL"
+	date        = "2021-07-26"
+	// invalidDate = "2021-07-269999999999"
+	// apiFormat   = "http://127.0.0.1:8080/price/%s/%s"
+)
+
 
 
 func (suite *serviceTestSuite) SetupTest() {
@@ -29,6 +39,32 @@ func (suite *serviceTestSuite) SetupTest() {
 	suite.service =  service.New(&zerolog.Logger{}, suite.repo, "Token doesn't exist", suite.client)
 	
     
+}
+
+func TestService(t *testing.T) {
+	suite.Run(t, new(serviceTestSuite))
+}
+
+
+
+func (suite *serviceTestSuite) TestServiceValid() {
+	price := &model.Price{
+		Open:  "99.9",
+		High:  "99.9",
+		Low:   "99.9",
+		Close: "99.9",
+	}
+
+	d, err := time.Parse("2021-07-26", date)
+	key := fmt.Sprintf("%s_%s", ticker, d)
+	suite.NoError(err)
+	
+	suite.repo.On("Load", key).Once().Return(price, true)
+	res, err := suite.service.GetPrice(ticker, d)
+	suite.NoError(err)
+	suite.Equal(res, price)
+
+
 }
 
 
